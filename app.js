a53dad01d296a24ad89cc1da5eccdfb08852e315
Ohad Ripsthos <105322785+Ripshtos@users.noreply.github.com>
2023-06-29 17:33:00 +0300

    added login page, session cookies, and logout logic

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
-var bodyParser = require("body-parser");
+
+nextSessionId = 1;
+
 const app = express();
 
 const Product = require("./models/product");
 const User = require("./models/users");
 
+
+var bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 //set view engine to ejs
 app.set('view engine', 'ejs');
-
+app.use(cookieParser()); // initializing the lib
 //set upp public directory to serve static files
 app.use(express.static('public'));
+
+// sessionID -> username
+const SESSIONS = {}
+
 
 //Initiate bodyParser to parse request body
 app.use(bodyParser.urlencoded({
@@ -45,7 +54,11 @@ app.get("/products/new", async (req, res) => {
 });
 
 app.get("/", async (req, res) => {
-  res.render("products/index");
+  const sessionId = req.cookies.sessionId;
+  const username = SESSIONS[sessionId];
+  const isLoggedIn = !!username; // Check if the username exists in SESSIONS
+  
+  res.render("products/index.ejs", { isLoggedIn });
 });
 
 app.post("/products/r", async (req, res) => {
@@ -55,13 +68,33 @@ app.post("/products/r", async (req, res) => {
 });
 
 app.post("/users/r", async (req, res) => {
-  const newUser = new User(req.body);
-  await newUser.save();
+  const useremail = req.body.email;
+
+  const user = await User.findOne({ email: useremail }); // Find a single user with the provided email
+ 
+  if (user) {
+    // If user with the email already exists, display an alert
+    res.send('<script>alert("User email already taken"); window.location.href = "/users/registration";</script>');
+    
+  }
+  else
+  {
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.redirect('/');
+  }
+
 });
 
 app.get("/users/registration", async (req, res) => {
   res.render("users/registration");
 });
+
+app.get("/users/login", async (req, res) => {
+  res.render("users/login");
+});
+
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
@@ -75,56 +108,50 @@ app.get("/product/:id", async (req, res) => {
   res.render("products/show", { product });
 });
 
+
+// routing for the login page
+app.post('/login', async (req, res) => {
+  const useremail = req.body.username;
+  const password = req.body.password;
+
+  const user = await User.findOne({ email: useremail }); // Find a single user with the provided email
+ 
+  console.log(user);
+
+  if (user && user.password === password) {
+    // Check if a user was found and the passwords match
+    res.cookie('sessionId', nextSessionId);
+    SESSIONS[nextSessionId] = useremail;
+    nextSessionId++;
+
+    app._router.stack.forEach(function(r){
+      if (r.route && r.route.path){
+        console.log(r.route.path)
+      }
+    })
+
+    res.redirect('/');
+  
+  }
+
+  else {
+    res.send('<script>alert("wrong email or password"); window.location.href = "/users/login";</script>');
+  }
+
+});
+
+// routing for logout
+app.get('/users/logout', (req, res) => {
+  const sessionId = req.cookies.sessionId;
+  // deleting the sessionId from temporary database
+  delete SESSIONS[sessionId];
+  // clearing the stored cookies sessionId
+  res.clearCookie('sessionId');
+  res.redirect('/');
+})
+
+
+
 app.listen(3000, () => {
   console.log("listening on port 3000!");
 });
-
-//************ */
-
-// app.use((req, res) => {
-
-//     console.log("we got a new request");
-//     res.send("<h1> we got your request ! thank you </h1>");
-// });
-//app.get('/', (req, res) => {
-
-//     res.render('home.ejs');
-// });
-// app.get('/search', (req, res) => {
-
-//     const { q } = req.query.split('&');
-//     if (!q) {
-//         res.send("error this is null");
-//     }
-//     else {
-//         res.send(`this is the ${q[0]}  and ${q[1]} page`);
-//     }
-
-// })
-
-// app.get('/r/:subreddit', (req, res) => {
-
-//     const { subreddit } = req.params;
-//     res.send(`this is my ${subreddit} page`);
-// });
-
-// app.get('/r/:subreddit/:postid', (req, res) => {
-
-//     const { subreddit, postid } = req.params;
-//     res.send(` this is the page of ${subreddit} and ${postid}`);
-// });
-
-// app.get('/cats', (req, res) => {
-
-//     res.send("<h1> we got your request ! thank you from cats  </h1>");
-// });
-
-// app.get('/dogs', (req, res) => {
-
-//     res.send("<h1> we got your request ! thank you from dogs </h1>");
-// });
-
-// app.get('*', (req, res) => {
-
-//     res.send("<h1> i dont know what you want  </h1>");
-// });
--- public/js/townapi.js
+++ public/js/townapi.js
@@ -1,24 +1,26 @@
-// Fetch cities from API and populate dropdown options
-  axios
-    .get('http://api.geonames.org/searchJSON', {
-      params: {
-        country: 'IL',
-        maxRows: 500,
-        username: 'ohad'
-      }
-    })
-    .then(response => {
-      const data = response.data;
-      const citySelect = document.getElementById('city');
+axios
+  .get('http://api.geonames.org/searchJSON', {
+    params: {
+      country: 'IL',
+      maxRows: 500,
+      username: 'ohad'
+    }
+  })
+  .then(response => {
+    const data = response.data;
+    const citySelect = document.getElementById('city');
 
-      // Loop through the API results and create option elements
-      data.geonames.forEach(city => {
-        const option = document.createElement('option');
-        option.value = city.name;
-        option.textContent = city.name;
-        citySelect.appendChild(option);
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching cities:', error);
+    // Sort the cities alphabetically
+    const sortedCities = data.geonames.sort((a, b) => a.name.localeCompare(b.name));
+
+    // Loop through the sorted API results and create option elements
+    sortedCities.forEach(city => {
+      const option = document.createElement('option');
+      option.value = city.name;
+      option.textContent = city.name;
+      citySelect.appendChild(option);
     });
+  })
+  .catch(error => {
+    console.error('Error fetching cities:', error);
+  });
